716d33d801979c08707f4ce774cbe24c658fd22c
Lillian Johnson <lillianjohnson@fb.com>
2021-05-27 15:37:09 -0700

    [package] turn MockZipReader into DirectoryReader and add test coverage
    ghstack-source-id: 11a7b393dc7f39d9ba3b91b092ab25a551147127
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59107

--- torch/package/package_importer.py
+++ torch/package/package_importer.py
@@ -13,6 +13,7 @@ from weakref import WeakValueDictionary
 import torch
 from torch.serialization import _get_restore_location, _maybe_decode_ascii
 
+from ._directory_reader import DirectoryReader
 from ._importlib import (
     _calc___package__,
     _normalize_line_endings,
@@ -21,7 +22,6 @@ from ._importlib import (
     _sanity_check,
 )
 from ._mangling import PackageMangler, demangle
-from ._mock_zipreader import MockZipReader
 from ._package_unpickler import PackageUnpickler
 from .file_structure_representation import Directory, _create_directory_from_file_list
 from .glob_group import GlobPattern
@@ -74,7 +74,7 @@ class PackageImporter(Importer):
             if not os.path.isdir(self.filename):
                 self.zip_reader = torch._C.PyTorchFileReader(self.filename)
             else:
-                self.zip_reader = MockZipReader(self.filename)
+                self.zip_reader = DirectoryReader(self.filename)
         else:
             self.filename = "<binary>"
             self.zip_reader = torch._C.PyTorchFileReader(file_or_buffer)
