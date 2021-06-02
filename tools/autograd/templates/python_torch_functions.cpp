a9b8a9064b7bf2a1775cff60bd74a8c5b91ae192
Yukio Siraichi <yukio.siraichi@gmail.com>
2021-06-02 18:41:37 +0900

    Implement NumPy-like `from_buffer` tensor constructor.
    Fixes #58549
    
    `from_buffer` constructs a tensor object from an already allocated buffer through
    CPython's buffer protocol. Besides the standard `dtype`, `count`, and `offset` parameters,
    this function also accepts:
    
    - `device`: where the buffer lives
    - `requires_grad`: should autograd record operations on the new tensor
    
    A new test file _test_buffer_protocol.py_ was created. Currently, only CPU tests were
    implemented. That's because neither PyTorch nor Numba implements CPython's buffer
    protocol. Therefore, there's no way to create a CUDA buffer with the existing
    dependencies (could use PyCUDA for that, though).
    
    At the moment, if `device` differs from the device the buffer actually lives, two things
    may happen:
    
    - `RuntimeError`, if `device='cuda'`
    - Segmentation fault (not tested -- see above), if `device='cpu'`
    
    Fixed failing CI tests.
    
    ghstack-source-id: 03f4be4a4c05d9fb28219f971572f88d69158e4f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/59077

--- tools/autograd/templates/python_torch_functions.cpp
+++ tools/autograd/templates/python_torch_functions.cpp
@@ -7,6 +7,7 @@
 // and also copied into 'torch' module.
 
 #include <Python.h>
+#include <pybind11/pybind11.h>
 
 // Undefine the copysign macro so that at::copysign works as intended with MSVC
 // https://github.com/python/cpython/blob/c60394c7fc9cc09b16e9675a3eeb5844b6d8523f/PC/pyconfig.h#L196
@@ -33,6 +34,7 @@
 
 #include <ATen/ATen.h>
 
+#include <fmt/format.h>
 #include <functional>
 #include <initializer_list>
 #include <stdexcept>
@@ -465,6 +467,102 @@ static PyObject * THPVariable_get_device(PyObject* self_, PyObject* args, PyObje
   END_HANDLE_TH_ERRORS
 }
 
+static PyObject * THPVariable_from_buffer(PyObject* self_, PyObject* args, PyObject* kwargs)
+{
+  HANDLE_TH_ERRORS
+  static PythonArgParser parser({
+    "from_buffer(PyObject* buffer, ScalarType dtype=None, int64_t count=-1, int64_t offset=0, *, Device? device=None, bool requires_grad=False)",
+  }, /*traceable=*/false);
+
+  PyObject* ret = nullptr;
+  ParsedArgs<6> parsed_args;
+  auto r = parser.parse(args, kwargs, parsed_args);
+
+  if (r.idx == 0) {
+    auto buffer = r.pyobject(0);
+    auto dtype = r.scalartype(1);
+    auto count = r.toInt64(2);
+    auto offset = r.toInt64(3);
+    auto device_opt = r.deviceOptional(4);
+    auto requires_grad = r.toBool(5);
+
+    auto elsize = at::elementSize(dtype);
+    size_t actual_count = 0;
+
+    Py_buffer view;
+    c10::optional<std::string> error_str = c10::nullopt;
+
+    if (PyObject_GetBuffer(buffer, &view, PyBUF_SIMPLE) < 0) {
+      return nullptr;
+    }
+
+    auto len = view.len;
+    auto buf = view.buf;
+    auto obj = view.obj;
+
+    Py_INCREF(obj);
+    PyBuffer_Release(&view);
+
+    if (len == 0 || count == 0) {
+      error_str = fmt::format(
+          "both buffer length ({}) and 'count' ({}) must not be empty",
+          len,
+          count);
+    } else if (offset < 0 || offset >= len) {
+      error_str = fmt::format(
+          "offset must be non-negative and no greater than buffer length"
+          "({}) minus 1, but got: {}",
+          len,
+          offset);
+    } else if (count < 0 && (len - offset) % elsize != 0) {
+      error_str = fmt::format(
+          "buffer size ({}) after offset ({}) must be a multiple of "
+          "element size ({})",
+          len,
+          offset,
+          elsize);
+    } else {
+      if (count < 0) {
+        actual_count = (len - offset) / elsize;
+      } else {
+        actual_count = static_cast<uint64_t>(count);
+      }
+
+      if (offset + actual_count * elsize > len) {
+        error_str = fmt::format(
+            "buffer has only {} elements after offset {}, but specified a size of {}",
+            (len - offset) / elsize,
+            offset,
+            actual_count);
+      } else {
+        auto offset_buf = static_cast<char*>(buf) + offset;
+        auto options = TensorOptions().dtype(dtype).device(device_opt);
+
+        auto tensor =
+            at::for_blob(offset_buf, static_cast<int64_t>(actual_count))
+                .options(options)
+                .deleter([=](void*) {
+                  pybind11::gil_scoped_acquire gil;
+                  Py_DECREF(obj);
+                })
+                .make_tensor();
+        tensor.requires_grad_(requires_grad);
+        ret = wrap(tensor);
+      }
+    }
+
+    if (error_str.has_value()) {
+      Py_DECREF(obj);
+      PyErr_SetString(PyExc_ValueError, error_str.value());
+    }
+  }
+
+  return ret;
+
+  Py_RETURN_NONE;
+  END_HANDLE_TH_ERRORS
+}
+
 static PyObject * THPVariable_numel(PyObject* self_, PyObject* args, PyObject* kwargs);
 
 // generated forward declarations start here
@@ -494,6 +592,7 @@ static PyMethodDef torch_functions[] = {
     METH_VARARGS | METH_KEYWORDS | METH_STATIC, NULL},
   {"dsmm", castPyCFunctionWithKeywords(THPVariable_mm), METH_VARARGS | METH_KEYWORDS | METH_STATIC, NULL},
   {"from_numpy", THPVariable_from_numpy, METH_STATIC | METH_O, NULL},
+  {"from_buffer", castPyCFunctionWithKeywords(THPVariable_from_buffer), METH_VARARGS | METH_KEYWORDS | METH_STATIC, NULL},
   {"full", castPyCFunctionWithKeywords(THPVariable_full), METH_VARARGS | METH_KEYWORDS | METH_STATIC, NULL},
   {"hsmm", castPyCFunctionWithKeywords(THPVariable_hspmm), METH_VARARGS | METH_KEYWORDS | METH_STATIC, NULL},
   {"nonzero", castPyCFunctionWithKeywords(THPVariable_nonzero), METH_VARARGS | METH_KEYWORDS | METH_STATIC, NULL},
